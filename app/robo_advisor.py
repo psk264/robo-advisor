931b9a92f27174f02db4108f065087c3e8e103ef
psk264 <84349071+psk264@users.noreply.github.com>
2021-06-10 20:15:47 -0400

    access data from alphavantage API

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,1 +1,15 @@
-print("TODO - make a web request for stock market data")
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="demo")
+symbol = "MSFT"
+
+request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}"
+
+response = requests.get(request_url)
+print(type(response))
+print(response.text)
+
